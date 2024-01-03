e85ec8ccf541e5665845dca8af0ef664b6a0eb9d
Muno Gwata <munozovepi@astute.money>
2024-01-03 13:13:57 +0200

    added the section responsiveness

--- src/app/page.js
+++ src/app/page.js
@@ -6,24 +6,23 @@ import customStyles from './styles/abstracts/_index.module.scss'
 export default function Home() {
   return (
     <main >
- <div className={customStyles.section}>
- <div className={customStyles['grid-container']}>
-  <div className={customStyles['grid-item']}>Column 1</div>
-  <div className={customStyles['grid-item']}>Column 2</div>
-  <div className={customStyles['grid-item']}>Column 3</div>
-  <div className={customStyles['grid-item']}>Column 4</div>
-  <div className={customStyles['grid-item']}>Column 5</div>
-  <div className={customStyles['grid-item']}>Column 6</div>
-  <div className={customStyles['grid-item']}>Column 7</div>
-  <div className={customStyles['grid-item']}>Column 8</div>
-  <div className={customStyles['grid-item']}>Column 9</div>
-  <div className={customStyles['grid-item']}>Column 10</div>
-  <div className={customStyles['grid-item']}>Column 11</div>
-  <div className={customStyles['grid-item']}>Column 12</div>
+<div className={customStyles.section}>
+    <div className={customStyles['grid-container']}>
+
+        {/* ... other grid items ... */}
+
+        {/* First div spanning 6 columns */}
+        <div className={`${customStyles['grid-item']} ${customStyles['span-6']}`}>Hello full width</div>
+
+        {/* Second div spanning 6 columns */}
+        <div className={`${customStyles['grid-item']} ${customStyles['span-6']}`}>Muno</div>
+
+    </div>
 </div>
 
 
-</div>
+
+
     </main>
   )
 }
